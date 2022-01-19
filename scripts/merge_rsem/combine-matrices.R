9819286bb02f15a6b5c2255f37a1d76e0835a2f1
Your Name <you@example.com>
2022-01-19 14:29:04 -0500

    :wrench: add collapsed para for combine the non-collapsed matrics

--- scripts/merge_rsem/combine-matrices.R
+++ scripts/merge_rsem/combine-matrices.R
@@ -1,15 +1,19 @@
 # Author: Komal S. Rathi
 # Function: Combine TPM matrices from various RNAseq datasets
-# TPM matrices should be collapsed to unique gene symbols
+# TPM matrices may or may not be collapsed to unique gene symbols
 
-suppressPackageStartupMessages(library(optparse))
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(plyr))
+suppressPackageStartupMessages({
+  library(optparse)
+  library(tidyverse)
+  library(plyr)
+})
 
 # parameters
 option_list <- list(
   make_option(c("--matrices"), type = "character",
               help = "Comma separated list of expression matrices to combine (RSEM TPM, FPM or Expected counts) (.RDS)"),
+  make_option(c("--collapsed"), type = "logical", default = FALSE,
+              help = "Are the input rds files collapsed? Default is FALSE"),
   make_option(c("--outfile"), type = "character",
               help = "Output filename (.RDS)"))
 
@@ -17,15 +21,22 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list = option_list))
 matrices <- opt$matrices
 matrices <- trimws(strsplit(matrices,",")[[1]]) 
+collapsed <- opt$collapsed
 outfile <- opt$outfile
 
 # function to merge all input datasets
 join.all <- function(...){
   x <- lapply(..., FUN = function(x) readRDS(x))
-  x <- lapply(x, FUN = function(x) x %>% rownames_to_column('gene'))# apply on each element on list and convert rownames to column
-  x <- join_all(x, by = 'gene', type = 'inner')
-  x <- x %>%
-    column_to_rownames('gene')
+  if(collapsed){
+    # if already collapsed to unique gene symbols do this
+    x <- lapply(x, FUN = function(x) x %>% rownames_to_column('gene'))# apply on each element on list and convert rownames to column
+    x <- join_all(x, by = 'gene', type = 'inner')
+    x <- x %>%
+      column_to_rownames('gene')
+  } else {
+    # if input files are not collapsed, do this
+    x <- join_all(x, by = 'gene_id', type = 'inner')
+  }
   return(x)
 }
 combined.mat <- join.all(matrices)
